8f1623c9a3fdba544d6ad7cd8caa39aa86d9992f
DonBatman <serfdon@gmail.com>
2015-04-28 10:17:26 -0700

    Added missing textures and set up groups

--- walls.lua
+++ walls.lua
@@ -24,18 +24,18 @@ local colbox_type4 = { --corner
 	}
 }
 
-local block_type1 = { -- desc2, typ, obj, colbox, drops
-{"Left", 		"left", 	"block1_top_l",		colbox_type1, "left"},
-{"Middle", 		"middle",	"block1_top_m",		colbox_type1, "middle"},
-{"Right", 		"right",	"block1_top_r",		colbox_type1, "right"},
-{"Corner", 		"corner",	"block1_top_c",		colbox_type4, "corner"},
-{"Column Top", 		"column",	"column_upper",		colbox_type2, "column"},
+local block_type1 = { -- desc2, typ, obj, colbox, drops, grup
+{"Left", 		"left", 	"block1_top_l",		colbox_type1, "left",   {cracky=2,not_in_creative_inventory=1}},
+{"Middle", 		"middle",	"block1_top_m",		colbox_type1, "middle", {cracky=2,not_in_creative_inventory=1}},
+{"Right", 		"right",	"block1_top_r",		colbox_type1, "right",  {cracky=2,not_in_creative_inventory=1}},
+{"Corner", 		"corner",	"block1_top_c",		colbox_type4, "corner", {cracky=2,not_in_creative_inventory=1}},
+{"Column Top", 		"column",	"column_upper",		colbox_type2, "column", {cracky=2,not_in_creative_inventory=1}},
 
-{"Left Bot", 		"bleft", 	"block1_bot_l",		colbox_type3, "left"},
-{"Middle Bot", 		"bmiddle",	"block1_bot_m",		colbox_type3, "middle"},
-{"Right Bot", 		"bright",	"block1_bot_r",		colbox_type3, "right"},
-{"Corner Bot", 		"bcorner",	"block1_bot_c",		colbox_type4, "corner"},
-{"Column Bot", 		"bcolumn",	"column_lower",		colbox_type2, "column"},
+{"Left Bot", 		"bleft", 	"block1_bot_l",		colbox_type3, "left",   {not_in_creative_inventory=1}},
+{"Middle Bot", 		"bmiddle",	"block1_bot_m",		colbox_type3, "middle", {not_in_creative_inventory=1}},
+{"Right Bot", 		"bright",	"block1_bot_r",		colbox_type3, "right",  {not_in_creative_inventory=1}},
+{"Corner Bot", 		"bcorner",	"block1_bot_c",		colbox_type4, "corner", {not_in_creative_inventory=1}},
+{"Column Bot", 		"bcolumn",	"column_lower",		colbox_type2, "column", {not_in_creative_inventory=1}},
 }
 for i in ipairs (block_type1) do
 	local desc2 = block_type1[i][1]
@@ -43,6 +43,7 @@ for i in ipairs (block_type1) do
 	local obj = block_type1[i][3]
 	local colbox = block_type1[i][4]
 	local drops = block_type1[i][5]
+	local grup = block_type1[i][6]
 
 
 local block_mat = { -- desc1, mat, img, img2
@@ -67,7 +68,7 @@ minetest.register_node('mylandscaping:rwall_'..typ..mat, {
 	drawtype = 'mesh',
 	mesh = 'mylandscaping_'..obj..'.obj',
 	tiles = {img..img2},
-	groups = {oddly_breakable_by_hand=2},
+	groups = grup,
 	paramtype = 'light',
 	paramtype2 = 'facedir',
 	drop = 'mylandscaping:rwall_'..drops..mat,
